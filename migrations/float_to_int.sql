5959647e6da60335e51ee518b6fbc7d367538529
init-iator <emuhnas1@hotmail.com>
2025-02-08 11:09:57 +0100

    floats_to_int

---
+++ migrations/float_to_int.sql
@@ -0,0 +1,70 @@
+-- step 2, fix datatypes in tables (float to int etc)
+begin transaction;
+update migrations set version = 2;
+-- Update script_lines table.
+CREATE TABLE new_script_lines (
+    id INTEGER NOT NULL, 
+    episode_id INTEGER NOT NULL, 
+    number INTEGER NOT NULL, 
+    raw_text VARCHAR NOT NULL, 
+    timestamp_in_ms INTEGER NOT NULL, 
+    speaking_line VARCHAR NOT NULL, 
+    character_id INTEGER, 
+    location_id INTEGER, 
+    raw_character_text VARCHAR, 
+    raw_location_text VARCHAR, 
+    spoken_words VARCHAR, 
+    normalized_text VARCHAR, 
+    word_count VARCHAR
+);
+INSERT INTO new_script_lines
+SELECT * FROM script_lines;
+DROP TABLE script_lines;
+ALTER TABLE new_script_lines RENAME TO script_lines;
+
+-- Update locations table.
+CREATE TABLE new_locations (
+    id INTEGER NOT NULL,
+    name VARCHAR NOT NULL,
+    normalized_name VARCHAR NOT NULL
+);
+INSERT INTO new_locations
+SELECT * FROM locations;
+DROP TABLE locations;
+ALTER TABLE new_locations RENAME TO locations;
+
+-- Update episodes table.
+CREATE TABLE new_episodes (
+    id INTEGER NOT NULL,
+    image_url VARCHAR,
+    imdb_rating FLOAT,
+    imdb_votes INTEGER,
+    number_in_season INTEGER NOT NULL,
+    number_in_series INTEGER NOT NULL,
+    original_air_date DATE NOT NULL,
+    original_air_year INTEGER NOT NULL,
+    production_code VARCHAR NOT NULL,
+    season INTEGER NOT NULL,
+    title VARCHAR NOT NULL,
+    us_viewers_in_millions FLOAT,
+    video_url VARCHAR,
+    views INTEGER
+);
+INSERT INTO new_episodes
+SELECT * FROM episodes;
+DROP TABLE episodes;
+ALTER TABLE new_episodes RENAME TO episodes;
+
+-- Update characters table.
+CREATE TABLE IF NOT EXISTS "new_characters" (
+    id INT NOT NULL,
+    name VARCHAR NOT NULL,
+    normalized_name VARCHAR NOT NULL,
+    gender VARCHAR
+);
+INSERT INTO new_characters
+SELECT * FROM characters;
+DROP TABLE characters;
+ALTER TABLE new_characters RENAME TO characters;
+
+commit;
